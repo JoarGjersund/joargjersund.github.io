27bdf46007de2b0f1149e9b26a16c25a1e23be2d
Joar Andreas Gjersund <joargjersund@gmail.com>
2018-06-10 19:08:02 +0200

    new snake game

---
+++ games/touchwipe.js
@@ -0,0 +1,40 @@
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(evt) {
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            direction= "r";
+        } else {
+
+            direction= "l";
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            direction= "u";
+        } else {
+            direction= "d";
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
